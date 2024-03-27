f3c37df61946882e2cf060c2d75585009123d510
Boyer Jennifer <jennifer.boyer@oclock.school>
2024-03-27 17:09:05 +0100

    feat : get one Article

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -1,5 +1,18 @@
-// import { Article } from "../models/associations.js";
+import { Article } from "../models/associations.js";
 
 export async function getAllArticles(req, res) {
-  res.send("Hello");
+  const articles = await Article.findAll({
+    attributes: ["id", "title", "description"],
+  });
+  res.json(articles);
+}
+
+export async function getOneArticle(req, res) {
+  const articleId = req.params.articleId;
+
+  const oneArticle = await Article.findByPk(articleId, {
+    attributes: ["id", "title", "description"],
+  });
+
+  res.json(oneArticle);
 }
--- src/routers/router.js
+++ src/routers/router.js
@@ -5,6 +5,8 @@ import * as articleController from "../controllers/articleController.js";
 import * as categoryController from "../controllers/categoryController.js";
 
 router.get("/articles", articleController.getAllArticles);
+router.get("/articles/:articleId", articleController.getOneArticle);
+
 router.get("/categories", categoryController.getAllCategories);
 
 router.use((req, res) => {
