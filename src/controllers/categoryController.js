231cdd47bf91eb2700611d9928cf9a8c30da9292
Boyer Jennifer <jennifer.boyer@oclock.school>
2024-03-27 18:13:14 +0100

    feat: CRUD category

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,5 +1,63 @@
-// import { Category } from "../models/associations.js";
+import { Category } from "../models/associations.js";
 
 export async function getAllCategories(req, res) {
-  res.send("Hello");
+  const categories = await Category.findAll({
+    attributes: ["id", "name"],
+    include: {
+      association: "articles",
+      attributes: ["title", "description"],
+    },
+  });
+  res.json(categories);
+}
+
+export async function getOneCategory(req, res) {
+  const categoryId = req.params.categoryId;
+
+  const oneCategory = await Category.findByPk(categoryId, {
+    attributes: ["id", "name"],
+    include: {
+      association: "articles",
+      attributes: ["title", "description"],
+    },
+  });
+
+  res.json(oneCategory);
+}
+
+export async function createCategory(req, res) {
+  const { name } = req.body;
+
+  const createdCategory = await Category.create({
+    name,
+  });
+
+  res.status(201).json(createdCategory);
+}
+
+export async function updateCategory(req, res) {
+  // trouver l'article a modifier = id
+  const categoryId = req.params.categoryId;
+
+  const category = await Category.findByPk(categoryId);
+
+  // Récupérer les données à modifier
+  const { name } = req.body;
+
+  // les update
+  category.name = name ?? category.name;
+
+  await category.save();
+
+  res.json(category);
+}
+
+export async function deleteCategory(req, res) {
+  const categoryId = req.params.categoryId;
+
+  const category = await Category.findByPk(categoryId);
+
+  await category.destroy();
+
+  res.status(204).end();
 }
--- src/routers/router.js
+++ src/routers/router.js
@@ -11,6 +11,10 @@ router.patch("/articles/:articleId", articleController.updateArticle);
 router.delete("/articles/:articleId", articleController.deleteArticle);
 
 router.get("/categories", categoryController.getAllCategories);
+router.get("/categories/:categoryId", categoryController.getOneCategory);
+router.post("/categories", categoryController.createCategory);
+router.patch("/categories/:categoryId", categoryController.updateCategory);
+router.delete("/categories/:categoryId", categoryController.deleteCategory);
 
 router.use((req, res) => {
   res.status(404).json("Not found");
