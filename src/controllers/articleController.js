aeb2b88d86285baf48eaf3f5173f4f9ddbfa909b
Boyer Jennifer <jennifer.boyer@oclock.school>
2024-03-31 16:38:04 +0200

    feat: validation user input

--- src/controllers/articleController.js
+++ src/controllers/articleController.js
@@ -1,4 +1,5 @@
-import { Article } from "../models/associations.js";
+import { Article, Category } from "../models/associations.js";
+import Joi from "joi";
 
 export async function getAllArticles(req, res) {
   const articles = await Article.findAll({
@@ -12,9 +13,13 @@ export async function getAllArticles(req, res) {
 }
 
 export async function getOneArticle(req, res) {
-  const articleId = req.params.articleId;
+  const id = +req.params.articleId;
 
-  const oneArticle = await Article.findByPk(articleId, {
+  if (!Number.isInteger(id)) {
+    return res.status(404).json({ error: "Not found" });
+  }
+
+  const oneArticle = await Article.findByPk(id, {
     attributes: ["id", "title", "description"],
     include: {
       association: "category",
@@ -22,14 +27,57 @@ export async function getOneArticle(req, res) {
     },
   });
 
+  if (!oneArticle) {
+    return res.status(404).json({ error: "Not found" });
+  }
   res.json(oneArticle);
 }
 
 export async function createArticle(req, res) {
   const { title, description, category_id } = req.body;
 
-  // TODO to handle error if category doesn't exist
-  // const categoryId = await Category.findByPk(category_id);
+  const createArticleSchema = Joi.object({
+    title: Joi.string()
+      .required()
+      .min(1)
+      .max(255)
+      .error(
+        () =>
+          new Error(
+            "The entry must be a string, not an empty field, have at least one character"
+          )
+      ),
+    description: Joi.string()
+      .required()
+      .min(1)
+      .error(
+        () =>
+          new Error(
+            "The entry must be a string, not an empty field, have at least one character"
+          )
+      ),
+    category_id: Joi.number()
+      .integer()
+      .required()
+      .greater(0)
+      .error(
+        () => new Error("The category ID must be an integer greater than zero")
+      ),
+  });
+
+  const validationBody = createArticleSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validationBody.error) {
+    return res.status(400).json({ error: validationBody.error.message });
+  }
+
+  const categoryId = await Category.findByPk(category_id);
+
+  if (!categoryId) {
+    return res.status(404).json({ error: "Not found" });
+  }
 
   const createdArticle = await Article.create({
     title,
@@ -41,18 +89,60 @@ export async function createArticle(req, res) {
 }
 
 export async function updateArticle(req, res) {
-  // trouver l'article a modifier = id
-  const articleId = req.params.articleId;
+  const id = +req.params.articleId;
 
-  const article = await Article.findByPk(articleId);
+  if (!Number.isInteger(id)) {
+    return res.status(404).json({ error: "Not found" });
+  }
 
-  // Récupérer les données à modifier
+  const article = await Article.findByPk(id);
+
+  if (!article) {
+    return res.status(404).json({ error: "Not found" });
+  }
+
   const { title, description, category_id } = req.body;
 
-  // TODO to handle error if category doesn't exist
-  // const categoryId = await Category.findByPk(category_id);
+  const updateArticleSchema = Joi.object({
+    title: Joi.string()
+      .min(1)
+      .max(255)
+      .error(
+        () =>
+          new Error(
+            "The entry must be a string, not an empty field, have at least one character"
+          )
+      ),
+    description: Joi.string()
+      .min(1)
+      .error(
+        () =>
+          new Error(
+            "The entry must be a string, not an empty field, have at least one character"
+          )
+      ),
+    category_id: Joi.number()
+      .integer()
+      .greater(0)
+      .error(
+        () => new Error("The category ID must be an integer greater than zero")
+      ),
+  });
 
-  // les update
+  const validationBody = updateArticleSchema.validate(req.body, {
+    abortEarly: false,
+  });
+
+  if (validationBody.error) {
+    return res.status(400).json({ error: validationBody.error.message });
+  }
+
+  const categoryId = await Category.findByPk(category_id);
+
+  if (!categoryId) {
+    return res.status(404).json({ error: "Not found" });
+  }
+
   article.title = title ?? article.title;
   article.description = description ?? article.description;
   article.category_id = category_id ?? article.category_id;
@@ -63,9 +153,17 @@ export async function updateArticle(req, res) {
 }
 
 export async function deleteArticle(req, res) {
-  const articleId = req.params.articleId;
+  const id = +req.params.articleId;
 
-  const article = await Article.findByPk(articleId);
+  if (!Number.isInteger(id)) {
+    return res.status(404).json({ error: "Not found" });
+  }
+
+  const article = await Article.findByPk(id);
+
+  if (!article) {
+    return res.status(404).json({ error: "Not found" });
+  }
 
   await article.destroy();
 
--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -1,4 +1,5 @@
 import { Category } from "../models/associations.js";
+import Joi from "joi";
 
 export async function getAllCategories(req, res) {
   const categories = await Category.findAll({
@@ -12,9 +13,13 @@ export async function getAllCategories(req, res) {
 }
 
 export async function getOneCategory(req, res) {
-  const categoryId = req.params.categoryId;
+  const id = +req.params.categoryId;
 
-  const oneCategory = await Category.findByPk(categoryId, {
+  if (!Number.isInteger(id)) {
+    return res.status(404).json({ error: "Not found" });
+  }
+
+  const oneCategory = await Category.findByPk(id, {
     attributes: ["id", "name"],
     include: {
       association: "articles",
@@ -22,11 +27,28 @@ export async function getOneCategory(req, res) {
     },
   });
 
+  if (!oneCategory) {
+    return res.status(404).json({ error: "Not found" });
+  }
+
   res.json(oneCategory);
 }
 
 export async function createCategory(req, res) {
   const { name } = req.body;
+
+  const createCategorySchema = Joi.object({
+    name: Joi.string().min(1).max(255).required(),
+  });
+
+  const validationBody = createCategorySchema.validate({ name });
+
+  if (validationBody.error) {
+    return res.status(400).json({
+      error:
+        "The entry must be a text, cannot be an empty field, have at least one character long",
+    });
+  }
 
   const createdCategory = await Category.create({
     name,
@@ -36,15 +58,33 @@ export async function createCategory(req, res) {
 }
 
 export async function updateCategory(req, res) {
-  // trouver l'article a modifier = id
-  const categoryId = req.params.categoryId;
+  const id = +req.params.categoryId;
 
-  const category = await Category.findByPk(categoryId);
+  if (!Number.isInteger(id)) {
+    return res.status(404).json({ error: "Not found" });
+  }
 
-  // Récupérer les données à modifier
+  const category = await Category.findByPk(id);
+
+  if (!category) {
+    return res.status(404).json({ error: "Not found" });
+  }
+
   const { name } = req.body;
 
-  // les update
+  const updateCategorySchema = Joi.object({
+    name: Joi.string().min(1).max(255),
+  });
+
+  const validationBody = updateCategorySchema.validate({ name });
+
+  if (validationBody.error) {
+    return res.status(400).json({
+      error:
+        "The entry must be a text, cannot be an empty field, have at least one character long",
+    });
+  }
+
   category.name = name ?? category.name;
 
   await category.save();
@@ -53,9 +93,17 @@ export async function updateCategory(req, res) {
 }
 
 export async function deleteCategory(req, res) {
-  const categoryId = req.params.categoryId;
+  const id = req.params.categoryId;
 
-  const category = await Category.findByPk(categoryId);
+  if (!Number.isInteger(id)) {
+    return res.status(404).json({ error: "Not found" });
+  }
+
+  const category = await Category.findByPk(id);
+
+  if (!category) {
+    return res.status(404).json({ error: "Not found" });
+  }
 
   await category.destroy();
 
